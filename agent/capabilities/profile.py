8973c59ac4c64103675673cb8698a8090ed9e824
Polychronis Ypodimatopoulos <ypodim@gmail.com>
2011-11-10 09:59:39 -0500

    tour

--- agent/capabilities/profile.py
+++ agent/capabilities/profile.py
@@ -62,7 +62,7 @@ class profile():
     
     def get_vals(self, key):
         print key
-        key = unicode(key, erros='replace')
+        key = unicode(key, errors='replace')
         print key
         return self.db.smembers(u'%s:profile:key:%s' % (self.usr, key))
     
