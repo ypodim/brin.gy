c67bb2c9b5fcf689626c35e7c03cf341916afde6
Pol <ypodim@gmail.com>
2012-08-06 22:31:30 -0400

    reorg

--- website/static/js/views/world.js
+++ website/static/js/views/world.js
@@ -259,6 +259,7 @@ define([
                         })
                     };
                     that.app.postNewContext(appdic, clb);
+                    
                 } else
                     that.showAllContexts({notoggle:true});
             });
